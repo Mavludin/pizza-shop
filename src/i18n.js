51acf315d680f3c7a06453e09f14680c7e360ff3
Mavludin <imevlud@gmail.com>
2021-07-26 14:08:30 +0300

    exluded the env filles

--- src/i18n.js
+++ src/i18n.js
@@ -9,39 +9,28 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const locizeOptions = {
   projectId: process.env.REACT_APP_LOCIZE_PROJECTID,
-  apiKey: process.env.REACT_APP_LOCIZE_APIKEY, // YOU should not expose your apps API key to production!!!
+  apiKey: process.env.REACT_APP_LOCIZE_APIKEY,
   referenceLng: process.env.REACT_APP_LOCIZE_REFLNG,
   version: process.env.REACT_APP_LOCIZE_VERSION
 };
 
-if (!isProduction) {
-  // locize-lastused
-  // sets a timestamp of last access on every translation segment on locize
-  // -> safely remove the ones not being touched for weeks/months
-  // https://github.com/locize/locize-lastused
-  i18n.use(locizeLastUsed);
-}
+if (!isProduction) i18n.use(locizeLastUsed);
 
 i18n
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .use(Backend)
   .use(locizeLastUsed)
   .use(locizePlugin)
   .init({
-    debug: false,
+    debug: !isProduction,
     fallbackLng: 'en',
     interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+      escapeValue: false,
     },
     backend: locizeOptions,
     locizeLastUsed: locizeOptions,
-    saveMissing: !isProduction // you should not use saveMissing in production
+    saveMissing: !isProduction
   });
 
 export default i18n;
